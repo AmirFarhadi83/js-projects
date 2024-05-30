f4fba73e7e30dd1d7521237757fc17ef5c61ba23
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-30 14:08:20 +0330

    Update script.js

--- 8 - Dictionary App (API)/js/script.js
+++ 8 - Dictionary App (API)/js/script.js
@@ -3,37 +3,38 @@
 const resultEL = document.querySelector(".result");
 const sound = document.querySelector("#sound");
 const searchBtnEl = document.querySelector("#search-btn");
-const wordInpEl = document.querySelector("#word-inp");
 
-searchBtnEl.addEventListener("click", async function () {
+searchBtnEl.addEventListener("click", async () => {
+  let wordInpEl = document.querySelector("#word-inp");
   try {
     const res = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInpEl}`
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInpEl.value}`
     );
     const data = await res.json();
+    console.log(data);
     resultEL.innerHTML = `<div class="word">
-      <h3>${wordInpEl}</h3>
+      <h3>${data[0]?.word}</h3>
       <button onclick='playSound()'>
         <i class="bx bxs-volume-full"></i>
       </button>
     </div>
     <div class="details">
-      <p>${data[0].meanings[0].partOfSpeech}</p>
-      <p>/${data[0].phonetic}/</p>
+      <p>${data[0]?.meanings[0]?.partOfSpeech}</p>
+      <p>/${data[0]?.phonetic}/</p>
     </div>
     <p class="word-meaning">
-      ${data[0].meanings[0].definitions[0].definition}
+      ${data[0]?.meanings[0]?.definitions[0]?.definition}
     </p>
     <p class="word-example">
-      ${data[0].meanings[0].definitions[0].example || ""}
+      ${data[0]?.meanings[0]?.definitions[0]?.example || ""}
     </p>`;
-    sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+    sound.setAttribute("src", `https:${data[0]?.phonetics[0]?.audio}`);
   } catch (error) {
     console.log(error);
     resultEL.innerHTML = `<h3 class='error'>Couldn't find the word</h3>`;
   }
 });
-
+console.log(sound);
 function playSound() {
   sound.play();
 }
