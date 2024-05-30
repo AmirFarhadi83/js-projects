2b71274d3c575725f1e8694e739ebf235d4ef555
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-30 11:49:01 +0330

    update closest

--- 1 - Burger Menu/js/script.js
+++ 1 - Burger Menu/js/script.js
@@ -1,13 +1,21 @@
-'use strict'
+"use strict";
 
-const sidebarEl = document.querySelector('.sidebar')
-const menuBtn=document.querySelector('.bx-menu')
-const closeBtn = document.querySelector('.bx-x')
+const sidebarEl = document.querySelector(".sidebar");
+const menuBtn = document.querySelector(".bx-menu");
+const closeBtn = document.querySelector("i.bx.bx-x");
 
-menuBtn.addEventListener('click', () => {
-    sidebarEl.style.display = 'flex';
-});
+// menuBtn.addEventListener("click", () => {
+//   sidebarEl.style.display = "flex";
+// });
 
-closeBtn.addEventListener('click', () => {
-    sidebarEl.style.display = 'none';
+// closeBtn.addEventListener("click", () => {
+//   sidebarEl.style.display = "none";
+// });
+
+window.addEventListener("click", function (e) {
+  if (e.target.closest(".bx-menu") || e.target.closest(".sidebar")) {
+    sidebarEl.style.display = "flex";
+  } else {
+    sidebarEl.style.display = "none";
+  }
 });
